0a2df7c07a2b946247021aa7ac4d93251f06dbc2
Ivan <isipro@gmail.com>
2022-04-15 13:07:54 +0300

    implemented fadeIn on scroll implemented load more button

--- js/projects.js
+++ js/projects.js
@@ -17,15 +17,33 @@
     new Project('Garage lifting and door change', 'project info','image1.jpg', 'image2.jpg'),
     new Project('Garden rejuvenation at Beverly Hills','project info', 'image1.jpg', 'image2.jpg'),
     new Project('House Painting Job','project info', 'image1.jpg', 'image2.jpg'),
+    new Project('Front Yard Renovation at Kasper','project info', 'image1.jpg', 'image2.jpg'),
+    new Project('Roof Fix at Aldi', 'project info', 'image1.jpg', 'image2.jpg'),
+    new Project('Garage lifting and door change', 'project info','image1.jpg', 'image2.jpg'),
+    new Project('Garden rejuvenation at Beverly Hills','project info', 'image1.jpg', 'image2.jpg'),
+    new Project('House Painting Job','project info', 'image1.jpg', 'image2.jpg'),
   ]
 
+  
+  let n = 0;
+  function loadProjects(){
+    if(n <= projects.length - 2) {
+      n += 2;
+    }
+    debugger;
+    return projects.slice(0, n); 
+  }
+  
+
+  // number to load
   
 
   function renderProjects(){
     let = projectsContents = ``;
+    let loadedProjects = loadProjects();
     let projectsList = document.querySelector('.projects-list');
 
-    for ( let project of projects){
+    for ( let project of loadedProjects){
       projectsContents += 
       `<div class="project hidden">
         <div class="box1">
@@ -49,11 +67,42 @@
   }
   renderProjects();
 
+
+  function isScrolledIntoView(el) {
+    var rect = el.getBoundingClientRect();
+    
+    var elemTop = rect.top;
+    var elemBottom = rect.bottom;
+
+    // Only completely visible elements return true:
+    var isVisible = (elemTop >= 0) && (elemBottom <= window.innerHeight);
+    // Partially visible elements return true:
+    //isVisible = elemTop < window.innerHeight && elemBottom >= 0;
+    return isVisible;
+}
+
+  
+
   function makeElVisible(){
+    let projectsEls = document.querySelectorAll('.project');
     console.log('making el visible');
 
+    for ( let le of projectsEls){
+
+      if(isScrolledIntoView(le)){
+        le.classList.remove('hidden');
+      } else {
+        le.classList.add('hidden');
+      }
+    }
   }
 
-  document.onscroll(makeElVisible())
+  window.onscroll = makeElVisible;
+
+  let loadMoreBtn = document.querySelector('.load-more-btn');
+  loadMoreBtn.addEventListener('click', ()=>{
+    loadProjects();
+    renderProjects();
+  });
 
 })()
